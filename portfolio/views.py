48ef5b85f00dbb4d7c2491aceb610bea15e4bd8c
jd <asefadoj@gmail.com>
2023-04-08 21:43:23 -0300

    proyecto en djangoby js

---
+++ portfolio/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+#from django.http import HttpResponse
+
+from .models import Project
+
+
+
+def home(request):
+    projects = Project.objects.all()
+    return render(request, 'home.html', {"projects": projects})
+
+
+#def home(request):
+#   return HttpResponse("Hello, world!")
